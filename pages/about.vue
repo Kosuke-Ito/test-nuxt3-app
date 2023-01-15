af23f28a2f5fe3d03e9f0148ac050716dba93013
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-01-16 00:58:43 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div>
+</template>
+<script setup>
+function enableCustomLayout() {
+  setPageLayout('custom');
+}
+definePageMeta({
+  layout: false,
+});
+</script>
