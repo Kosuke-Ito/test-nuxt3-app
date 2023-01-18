35e48ac6427ea228c83a089bfe64087579e19a39
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-01-19 04:01:38 +0900

    assetsに画像を追加し、表示

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <h1>Main Page</h1>
     <button @click="handleClick">Coupon Get</button>
     <LazyCoupon v-if="show" />
+    <div>
+      <img src="~/assets/icon.png" alt="Nuxt3 Icon" />
+    </div>
   </div>
 </template>
 <script setup>
