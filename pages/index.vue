6dc4a12e4f0b25026c5281df60c7e8298196e6c2
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-01-22 18:19:00 +0900

    composablesディレクトリを作成し、useCounter関数を作成

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,16 @@
     <div>
       <img src="/icon.png" alt="Nuxt3 Icon" />
     </div>
+    <div>Count:{{ count }}</div>
+    <div>
+      <button @click="() => inc()">increase</button>
+      <button @click="() => dec()">decrease</button>
+    </div>
   </div>
 </template>
 <script setup>
 const show = ref(false);
+const { count, inc, dec } = useCounter(100);
 
 const handleClick = () => {
   show.value = true;
