c5abc8c4ca0a44766d0047f48c195474ead2458e
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-02-12 15:42:53 +0900

    postsページへのリンクをnavbarに追加

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,6 +4,7 @@
     <NuxtLink class="nav-item" href="/about">About</NuxtLink>
     <NuxtLink class="nav-item" :to="{ name: 'users-list' }">User List</NuxtLink>
     <NuxtLink class="nav-item" to="https://google.com" target="_blank">Google</NuxtLink>
+    <NuxtLink to="/posts">Post List</NuxtLink>
   </nav>
 </template>
 
