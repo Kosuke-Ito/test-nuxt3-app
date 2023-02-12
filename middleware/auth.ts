6260605eb90aa75cc12bce2d2c063dd176f88cf4
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-02-12 13:57:36 +0900

    ルートパスからaboutページにアクセスした場合、ルートにリダイレクトする

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('from', from);
-  console.log('to', to);
+  if (from.fullPath === '/') {
+    return navigateTo('/');
+  }
 });
