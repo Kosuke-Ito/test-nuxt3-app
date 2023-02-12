adcb6ceca454337b3ef325e9e5bb1a7aa8452a5b
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-02-12 15:29:30 +0900

    個別でメタを設定

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -4,6 +4,13 @@
 
 <script setup>
 const router = useRoute();
-
-console.log(router.params.id);
+useHead({
+  title: 'Nuxt 3',
+  meta: [
+    {
+      name: 'description',
+      content: `User Id: ${router.params.id}`,
+    },
+  ],
+});
 </script>
