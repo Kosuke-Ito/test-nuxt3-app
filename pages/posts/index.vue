5d40a34298dff55ef20d047dda27b50822a3cfca
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-02-21 02:31:13 +0900

    useLazyFetchでデータ取得

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,17 +2,19 @@
 const {
   data: posts,
   error,
-  refresh,
-} = await useFetch('https://jsonplaceholder.typicode.com/posts/');
+  pending,
+} = await useLazyFetch('https://jsonplaceholder.typicode.com/posts/');
 </script>
 
 <template>
   <div>
     <h1>Posts一覧</h1>
-    <button @click="refresh()">再取得</button>
     <p v-if="error">{{ error }}</p>
+    <p v-if="pending">Loading...</p>
     <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+      </li>
     </ul>
   </div>
 </template>
