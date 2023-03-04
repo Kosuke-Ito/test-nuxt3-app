517f04978a3953b77644c601dea851adb1d8df98
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-03-05 02:19:28 +0900

    aboutにcounterを追加

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <button @click="enableCustomLayout">Update layout</button>
+    <Head>
+      <Title>Aboutページ</Title>
+      <Meta name="description" content="Aboutページ" />
+    </Head>
+    <h1>About Page</h1>
+    <h2>Counter</h2>
+    <p>Count: {{ counter }}</p>
+    <div>
+      <button @click="counter++">+</button>
+      <button @click="enableCustomLayout">Update layout</button>
+    </div>
   </div>
 </template>
 <script setup>
 const title = ref('Aboutページ');
 const description = ref('Aboutページ');
+const counter = useState('counter', () => 0);
 
 useHead({
   title,
