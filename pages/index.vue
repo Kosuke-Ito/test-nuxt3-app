ffb8a16588ccc409a723d6b0a945f0a0ded34321
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-03-12 21:20:41 +0900

    簡単なAPIの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,10 @@
 <template>
   <div>
     <h1>Main Page</h1>
-    <h2>{{ $hello('World') }}</h2>
-    <button @click="handleClick">Coupon Get</button>
-    <LazyCoupon v-if="show" />
-    <div>
-      <img src="~/assets/icon.png" alt="Nuxt3 Icon" />
-    </div>
-    <div>
-      <img src="/icon.png" alt="Nuxt3 Icon" />
-    </div>
-    <div>Count:{{ count }}</div>
-    <div>
-      <button @click="() => inc()">increase</button>
-      <button @click="() => dec()">decrease</button>
-    </div>
+    <h2>{{ data }}</h2>
   </div>
 </template>
-<script setup>
-const show = ref(false);
-const { count, inc, dec } = useCounter(100);
-const { $hello } = useNuxtApp();
 
-const handleClick = () => {
-  show.value = true;
-};
+<script setup>
+const { data } = useFetch('/api/hello');
 </script>
