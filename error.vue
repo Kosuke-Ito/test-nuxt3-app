0d6115dd73543598052b78b0cbc636da4ab2bad1
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-03-12 17:49:30 +0900

    errorページを追加

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <p>{{ error }}</p>
+    <button @click="clearError({ redirect: '/' })">Clear Error</button>
+    
+    <button @click="clearError">Clear Error2</button>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+const props = defineProps({
+  error: Object,
+});
+</script>
